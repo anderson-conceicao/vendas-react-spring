948547fc1a10647943cbafd3f24dd2ba6d1e17bb
Anderson Silva da Conceição <anderson.conceicao@gmail.com>
2021-09-08 20:55:37 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/anderson-conceicao" target="_blank" rel="noreferrer">Anderson Silva da Conceição</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
